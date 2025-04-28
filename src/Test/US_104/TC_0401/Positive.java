0c6caf2d05d0fa12cf75fe38144fbe58fe198352
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-04-28 20:14:41 +0300

    SuccessfulLogin

---
+++ src/Test/US_104/TC_0401/Positive.java
@@ -0,0 +1,13 @@
+package Test.US_104.TC_0401;
+
+import Utlity.BaseDriverLogin;
+import org.junit.Test;
+
+public class Positive extends BaseDriverLogin {
+
+    @Test
+    public void SuccessfulLogin(){
+
+        BekleKapat();
+    }
+}
