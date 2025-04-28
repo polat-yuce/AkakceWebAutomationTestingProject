6f7907ceb0937734238be37a893859ff7eb0c7a1
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-04-28 19:50:58 +0300

    us2_1

---
+++ src/Test/US_102/TC_0201/Positive.java
@@ -0,0 +1,29 @@
+package Test.US_102.TC_0201;
+
+import Utlity.BaseDriver;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Positive extends BaseDriver {
+
+    @Test
+    public void AccountVerificationCheck() {
+
+        driver.get("https://www.akakce.com/");
+
+        driver.findElement(By.xpath("//a[text()='Giriş Yap']")).click();
+
+        driver.findElement(By.id("life")).sendKeys("leomessi@gmail.com");
+        driver.findElement(By.id("lifp")).sendKeys("Leo1234@");
+
+        driver.findElement(By.id("lfb")).click();
+
+        WebElement isim = driver.findElement(By.xpath("//a[text()='Lionel']"));
+
+        Assert.assertTrue("Hesap açılmadı", isim.getText().equalsIgnoreCase("Lionel"));
+
+        BekleKapat();
+    }
+}
